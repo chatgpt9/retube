5b9a279c41868f8cd3160e6a93fc30e456a02eee
chatgpt9 <128497084+chatgpt9@users.noreply.github.com>
2023-03-24 13:38:01 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,60 +1,62 @@
 import os
 from playwright.sync_api import Playwright, sync_playwright
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
-VIDEO_INDEX_URL = "https://www.youtube.com/channel/UCBR8-60-B28hp2BmDPdntcQ/videos"
-VIDEO_SELECTOR = "#video-title"
-VIDEO_DOWNLOAD_URL = "https://www.onlinevideoconverter.com/youtube-converter"
-VIDEO_DOWNLOAD_SELECTOR = "#texturl"
-VIDEO_DOWNLOAD_BUTTON = "//button[contains(@class, 'btn-primary')]"
-FINAL_VIDEO_PATH = "/path/to/final.mp4"
-GMAIL_USERNAME = os.environ.get("GMAIL_USERNAME")
-GMAIL_PASSWORD = os.environ.get("GMAIL_PASSWORD")
+# Set environment variables for Gmail and Youtube credentials
+gmail_username = os.getenv('GMAIL_USERNAME')
+gmail_password = os.getenv('GMAIL_PASSWORD')
+youtube_username = os.getenv('YOUTUBE_USERNAME')
+youtube_password = os.getenv('YOUTUBE_PASSWORD')
 
-def download_video():
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=True)
-        page = browser.new_page()
-        page.goto(VIDEO_INDEX_URL)
-        video_title = page.query_selector(VIDEO_SELECTOR).text_content()
-        page.click(VIDEO_SELECTOR)
-        page.wait_for_selector(VIDEO_DOWNLOAD_SELECTOR)
-        page.fill(VIDEO_DOWNLOAD_SELECTOR, page.url)
-        page.click(VIDEO_DOWNLOAD_BUTTON)
-        page.wait_for_navigation()
-        page.click("[name='submit']")
-        page.wait_for_selector("[aria-describedby='download-result']")
-        download_url = page.query_selector("[aria-describedby='download-result'] a").get_attribute("href")
-        page.close()
-        browser.close()
-    os.system(f"wget -O {FINAL_VIDEO_PATH} {download_url}")
-    print(f"Downloaded {video_title} as final.mp4")
+# Define function to download video from Youtube index page
+def download_video(page):
+    # Navigate to Youtube index page
+    page.goto('https://www.youtube.com')
+    # Find the first video link and click on it
+    video_link = page.locator('#thumbnail').first()
+    video_link.click()
+    # Wait for video to load and click on download button
+    page.wait_for_selector('#buttons .ytp-download-button')
+    download_button = page.locator('#buttons .ytp-download-button')
+    download_button.click()
+    # Wait for download to finish and return the downloaded file path
+    page.wait_for_download()
+    return page.context().downloads_folder().get_downloaded_file_path()
 
-def upload_video():
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--disable-extensions")
-    driver = webdriver.Chrome(options=options)
-    driver.get("https://www.youtube.com/upload")
-    driver.find_element(By.ID, "identifierId").send_keys(GMAIL_USERNAME + Keys.ENTER)
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password")))
-    driver.find_element(By.NAME, "password").send_keys(GMAIL_PASSWORD + Keys.ENTER)
-    driver.find_element(By.NAME, "Filedata").send_keys(FINAL_VIDEO_PATH)
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "privacy-public")))
-    driver.find_element(By.ID, "privacy-public").click()
-    driver.find_element(By.ID, "next-button").click()
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "done-button")))
-    driver.find_element(By.ID, "done-button").click()
-    driver.quit()
-    print(f"Uploaded {FINAL_VIDEO_PATH} to YouTube")
+# Define function to upload video to Youtube
+def upload_video(page, video_path):
+    # Navigate to Youtube upload page
+    page.goto('https://www.youtube.com/upload')
+    # Fill in Youtube login credentials and submit
+    page.fill('#identifierId', youtube_username)
+    page.click('#identifierNext')
+    page.fill('#password input', youtube_password)
+    page.click('#passwordNext')
+    # Wait for upload page to load and select file to upload
+    page.wait_for_selector('#upload-prompt-box input[type="file"]')
+    upload_input = page.locator('#upload-prompt-box input[type="file"]')
+    upload_input.set_input_files(video_path)
+    # Wait for video to upload and fill in title and description
+    page.wait_for_selector('#textbox')
+    page.fill('#textbox', 'My uploaded video')
+    page.fill('#description', 'This is my first uploaded video')
+    # Click on publish button
+    page.click('#publish-button')
 
-if __name__ == "__main__":
-    download_video()
-    upload_video()
+# Run the script
+with sync_playwright() as playwright:
+    # Set up browser and page context
+    browser = playwright.chromium.launch(headless=True)
+    context = browser.new_context()
+    page = context.new_page()
+    # Log in to Gmail
+    page.goto('https://www.gmail.com')
+    page.fill('#identifierId', gmail_username)
+    page.click('#identifierNext')
+    page.fill('#password input', gmail_password)
+    page.click('#passwordNext')
+    # Download video and upload it to Youtube
+    video_path = download_video(page)
+    upload_video(page, video_path)
+    # Clean up resources
+    context.close()
+    browser.close()
