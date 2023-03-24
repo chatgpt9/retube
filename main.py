236a25406793d0edc4d7d7447a89c387e025a155
chatgpt9 <128497084+chatgpt9@users.noreply.github.com>
2023-03-24 12:47:18 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,53 +1,61 @@
 import os
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from playwright.sync_api import Playwright, sync_playwright
 
-async def download_video():
-    async with sync_playwright() as p:
-        browser = await p.chromium.launch(headless=True)
-        page = await browser.new_page()
-        await page.goto("https://www.youtube.com/")
-        await page.click("#search")
-        await page.type("#search", "daily one video")
-        await page.press("#search", "Enter")
-        await page.click("#contents #thumbnail")
-        await page.click("#meta-contents #subscribe-button")
-        await page.click("#meta-contents #menu-container #top-level-buttons #button")
-        await page.click("#items #items #label")
-        await page.click("#items #items #button")
-        await page.wait_for_selector("#dialog #confirm-button")
-        await page.click("#dialog #confirm-button")
-        await page.wait_for_selector("#progress #progress-text")
+# Set up environment variables
+email = os.environ.get('GMAIL_EMAIL')
+password = os.environ.get('GMAIL_PASSWORD')
 
-        # Rename downloaded file as final.mp4
-        downloaded_file = os.path.expanduser("~/Downloads/video.mp4")
-        final_file = os.path.expanduser("~/Downloads/final.mp4")
-        os.rename(downloaded_file, final_file)
+# Initialize Playwright
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=True)
+    page = browser.new_page()
 
-    await upload_video(final_file)
+    # Navigate to YouTube index page
+    page.goto('https://www.youtube.com')
 
-async def upload_video(final_file):
-    async with sync_playwright() as p:
-        browser = await p.chromium.launch(headless=True)
-        page = await browser.new_page()
-        await page.goto("https://www.youtube.com/upload")
-        await page.wait_for_selector("#start-upload #file-upload-button")
-        file_input = await page.query_selector("#start-upload #file-upload-button")
-        await file_input.set_input_files(final_file)
-        await page.wait_for_selector("#start-upload #next-button")
-        await page.click("#start-upload #next-button")
-        await page.wait_for_selector("#video-properties #title")
-        await page.fill("#video-properties #title", "Daily video upload")
-        await page.click("#video-properties #privacy-button")
-        await page.click("#video-properties #privacy-menu .ytp-menuitem:nth-child(1)")
-        await page.wait_for_selector("#video-properties #publish-button")
-        await page.click("#video-properties #publish-button")
-        await page.wait_for_selector("#upload-progress #text")
-        print("Video uploaded successfully.")
+    # Wait for page to load
+    page.wait_for_selector('#search')
 
-if __name__ == "__main__":
-    # Set environment variables for Gmail and password
-    os.environ["GMAIL_USERNAME"] = "chatgpt8k@gmail.com"
-    os.environ["GMAIL_PASSWORD"] = "tJ19dQzWWH4THIWb"
+    # Get the first video from the index page
+    video = page.query_selector('#contents ytd-rich-item-renderer')
 
-    # Call the function to download and upload video
-    download_video()
+    # Get the video link and title
+    video_link = video.query_selector('#thumbnail').get_attribute('href')
+    video_title = video.query_selector('#title').text_content()
+
+    # Download the video
+    with open('final.mp4', 'wb') as f:
+        f.write(page.goto(video_link).body())
+
+    # Quit Playwright
+    browser.close()
+
+# Initialize Selenium
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+
+# Log in to YouTube
+driver.get('https://accounts.google.com/signin')
+driver.find_element_by_id('identifierId').send_keys(email)
+driver.find_element_by_id('identifierNext').click()
+time.sleep(1)
+driver.find_element_by_name('password').send_keys(password)
+driver.find_element_by_id('passwordNext').click()
+time.sleep(1)
+
+# Upload the video to YouTube
+driver.get('https://www.youtube.com/upload')
+driver.find_element_by_id('file-picker').send_keys(os.path.abspath('final.mp4'))
+driver.find_element_by_id('textbox').send_keys(video_title)
+driver.find_element_by_id('next-button').click()
+time.sleep(1)
+driver.find_element_by_id('next-button').click()
+time.sleep(1)
+driver.find_element_by_id('done-button').click()
+
+# Quit Selenium
+driver.quit()
