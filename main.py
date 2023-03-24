fabb2cff39101f68ada971c7dcf737e08e22fe4d
chatgpt9 <128497084+chatgpt9@users.noreply.github.com>
2023-03-24 13:15:30 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,78 +1,60 @@
 import os
-import time
-from selenium import webdriver
 from playwright.sync_api import Playwright, sync_playwright
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+VIDEO_INDEX_URL = "https://www.youtube.com/channel/UCBR8-60-B28hp2BmDPdntcQ/videos"
+VIDEO_SELECTOR = "#video-title"
+VIDEO_DOWNLOAD_URL = "https://www.onlinevideoconverter.com/youtube-converter"
+VIDEO_DOWNLOAD_SELECTOR = "#texturl"
+VIDEO_DOWNLOAD_BUTTON = "//button[contains(@class, 'btn-primary')]"
+FINAL_VIDEO_PATH = "/path/to/final.mp4"
+GMAIL_USERNAME = os.environ.get("GMAIL_USERNAME")
+GMAIL_PASSWORD = os.environ.get("GMAIL_PASSWORD")
 
-# Set up the Playwright browser
-def setup_browser() -> Playwright:
+def download_video():
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
-        context = browser.new_context()
-        page = context.new_page()
-        return p, page
-
-
-# Log in to YouTube using Selenium
-def login(driver: webdriver.Chrome):
-    driver.get("https://www.youtube.com")
-    time.sleep(5) # wait for page to load
-    driver.find_element_by_xpath("//yt-formatted-string[text()='Sign in']").click()
-    time.sleep(5) # wait for page to load
-    driver.find_element_by_name("identifier").send_keys(os.environ.get("GMAIL"))
-    driver.find_element_by_id("identifierNext").click()
-    time.sleep(5) # wait for page to load
-    driver.find_element_by_name("password").send_keys(os.environ.get("GMAIL_PASSWORD"))
-    driver.find_element_by_id("passwordNext").click()
-    time.sleep(5) # wait for page to load
-
-
-# Download video from the index page using Playwright
-def download_video(page):
-    page.goto("https://www.youtube.com")
-    time.sleep(5) # wait for page to load
-    video = page.locator("//ytd-rich-item-renderer").locator("a[id='thumbnail']").locator("img")
-    video_title = video.get_attribute("alt")
-    video.click()
-    time.sleep(10) # wait for video to load
-    page.locator("button.ytp-fullscreen-button").click() # enter fullscreen
-    time.sleep(5) # wait for video to load
-    page.keyboard.press("d") # download video
-    time.sleep(10) # wait for video to download
-    return video_title
-
-
-# Upload video to your YouTube channel using Selenium
-def upload_video(driver: webdriver.Chrome, video_title: str):
-    driver.get("https://www.youtube.com/upload")
-    time.sleep(10) # wait for page to load
-    driver.find_element_by_name("Filedata").send_keys(os.path.abspath("final.mp4"))
-    time.sleep(10) # wait for upload to complete
-    driver.find_element_by_name("title").send_keys(video_title)
-    driver.find_element_by_name("privacy").click() # choose video privacy
-    driver.find_element_by_xpath("//div[text()='Public']").click()
-    driver.find_element_by_xpath("//span[text()='Publish']").click()
-    time.sleep(10) # wait for video to upload
-
-
-# Main function
-if __name__ == "__main__":
-    while True:
-        # Set up browser and login
-        browser, page = setup_browser()
-        driver = webdriver.Chrome()
-        login(driver)
-
-        # Download video and save as final.mp4
-        video_title = download_video(page)
+        page = browser.new_page()
+        page.goto(VIDEO_INDEX_URL)
+        video_title = page.query_selector(VIDEO_SELECTOR).text_content()
+        page.click(VIDEO_SELECTOR)
+        page.wait_for_selector(VIDEO_DOWNLOAD_SELECTOR)
+        page.fill(VIDEO_DOWNLOAD_SELECTOR, page.url)
+        page.click(VIDEO_DOWNLOAD_BUTTON)
+        page.wait_for_navigation()
+        page.click("[name='submit']")
+        page.wait_for_selector("[aria-describedby='download-result']")
+        download_url = page.query_selector("[aria-describedby='download-result'] a").get_attribute("href")
         page.close()
         browser.close()
-        os.rename(os.path.expanduser("~/Downloads/{}.mp4".format(video_title)), "final.mp4")
+    os.system(f"wget -O {FINAL_VIDEO_PATH} {download_url}")
+    print(f"Downloaded {video_title} as final.mp4")
 
-        # Upload video to YouTube
-        driver = webdriver.Chrome()
-        upload_video(driver, video_title)
-        driver.quit()
+def upload_video():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-extensions")
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://www.youtube.com/upload")
+    driver.find_element(By.ID, "identifierId").send_keys(GMAIL_USERNAME + Keys.ENTER)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password")))
+    driver.find_element(By.NAME, "password").send_keys(GMAIL_PASSWORD + Keys.ENTER)
+    driver.find_element(By.NAME, "Filedata").send_keys(FINAL_VIDEO_PATH)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "privacy-public")))
+    driver.find_element(By.ID, "privacy-public").click()
+    driver.find_element(By.ID, "next-button").click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "done-button")))
+    driver.find_element(By.ID, "done-button").click()
+    driver.quit()
+    print(f"Uploaded {FINAL_VIDEO_PATH} to YouTube")
 
-        # Wait for an hour before repeating the process
-        time.sleep(60*60)
+if __name__ == "__main__":
+    download_video()
+    upload_video()
